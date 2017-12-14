d7dac98332a64a7812b21f160b9604da0c4baa84
leafzsy <920733871@qq.com>
2017-12-14 18:04:41 +0800

    插值

--- python/py_biji.py
+++ python/py_biji.py
@@ -11,4 +11,14 @@ x=pd.Series([1,1,2,3,4])
 x.unique()
 np.unique(x)
 
-###
+###主成分分析
+from sklearn.decomposition import PCA
+D=np.random.rand(10,4)#生成随机矩阵
+pca=PCA(n_components='mle')#自动确定保留的特征数
+pca.fit(D)
+
+from pylab import *
+plot(pca.explained_variance_ratio_)# 方差百分比
+eigenvector=pca.components_ #返回模型特征向量#方差百分比
+D_pca=D*eigenvector
+####

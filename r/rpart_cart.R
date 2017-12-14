be10231172d574c9cdf7914cd74f2822ba56fe50
leafzsy <920733871@qq.com>
2017-12-14 16:29:39 +0800

    rpart of fashion-miner

---
+++ r/rpart_cart.R
@@ -0,0 +1,27 @@
+
+rm(list=ls())
+setwd('E:/cart')
+library(rpart.plot)
+library(rpart)
+library(readr)
+
+X_train<- read_csv('X_train.csv',col_names = FALSE)
+Y_train<- read_csv('y_train.csv')
+
+Y_train<-Y_train+1
+x_train<-X_train[1:60000,]
+y_train<-Y_train[1:60000,1]
+fit <- rpart(y_train~., data=cbind(x_train,y_train), method="class")
+rpart.plot(fit, compress = TRUE)
+
+X_test<- read_csv('X_test.csv',col_names = FALSE)
+y_test<- read_csv('y_test.csv')
+y_test<-y_test+1
+
+#X_test<-X_test[1:2000,]
+#y_test<-y_test[1:2000,1]
+
+result <- predict(fit,X_test,type="class") 
+source("count_result.R")  
+
+count_result(result,cbind(y_test))

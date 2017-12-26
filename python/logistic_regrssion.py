f773a20bcd64ebc79730db2b1a88fb82b6ed3db8
leafzsy <920733871@qq.com>
2017-12-26 17:58:57 +0800

    增加混淆矩阵可视化,训练集也是测试集

--- python/logistic_regrssion.py
+++ python/logistic_regrssion.py
@@ -9,15 +9,13 @@ from sklearn.linear_model import LogisticRegression as LR
 #参数初始化
 filename = 'E:/git/learn/python/data/bankloan.xls'
 data = pd.read_excel(filename)
-X = np.array(data.iloc[:500,:8])#dataframe 转 ndarray
-y = np.array(data.iloc[:500,8])#dataframe 转 ndarray
+X = np.array(data.iloc[:,:8])#dataframe 转 ndarray
+y = np.array(data.iloc[:,8])#dataframe 转 ndarray
 
-x_test=np.array(data.iloc[500:,:8])#dataframe 转 ndarray
-y_test=np.array(data.iloc[500:,8])
 
 #1、随机逻辑回归选取特征向量
-#模型的平均正确率为：0.816
-#测试集准确率为0.805
+#模型的平均正确率为：0.814
+#测试集准确率为0.814
 from sklearn.linear_model import RandomizedLogisticRegression as RLR
 rlr = RLR() #建立随机逻辑回归模型，筛选变量
 rlr.fit(X, y) #训练模型
@@ -30,9 +28,9 @@ x = X[:,b] #筛选好特征
 # #2、递归特征消除法，返回特征选择后的数据
 # #参数estimator为基模型
 # #参数n_features_to_select为选择的特征个数
-# #测试集预测准确率0.8
+# #测试集预测准确率0.814
 # from sklearn.feature_selection import RFE
-# b=RFE(estimator=LR(), n_features_to_select=3).fit(X, y).support_##特征选取向量
+# b=RFE(estimator=LR(), n_features_to_select=4).fit(X, y).support_##特征选取向量
 # print(u'有效特征为：%s' % ','.join(data.columns[:8][b]))
 # x=X[:,b]
 
@@ -43,6 +41,9 @@ print('模型训练结束。')
 print('模型的平均正确率为：%s' % lr.score(x, y))
 #给出模型的平均正确率，随机逻辑回归模型为80.6%
 
-Y=lr.predict(x_test[:,b])#测试集预测类别
-y_proba=lr.predict_proba(x_test[:,b])#测试集预测概率
-print(int(sum(Y==y_test))/len(y_test))
+Y=lr.predict(x)#测试集预测类别
+y_proba=lr.predict_proba(x)#测试集预测概率
+print(int(sum(Y==y))/len(y))
+
+from cm_plot import * #导入自行编写的混淆矩阵可视化函数
+cm_plot(y,Y).show()

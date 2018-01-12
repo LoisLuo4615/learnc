8e50e15c2e221cb005e21f3a8b21602cb0e0b79d
leaf <920733871@qq.com>
2018-01-12 16:36:59 +0800

    条形码 邮编 转换

--- c/c10-13.cpp
+++ c/c10-13.cpp
@@ -50,7 +50,6 @@ void posthet::check_code()
   while(code_char[i]!='\0')
     {
       code[i]=char_int(code_char[i]);
-      //cout << !isdigit(code_char[i]);
       if(!isdigit(code_char[i]))
           {
             cout << "The code is not int!\n";
@@ -59,7 +58,6 @@ void posthet::check_code()
       i++;
     }
   if(strlen(code_char)==27)
-  // if(code_char[27]=='\0' && code_char[5]!='\0')
     {
       for(int i=1;i<=21;i=i+5)//每组有2个1,例：11000
         if((code[i]+code[i+1]+code[i+2]+code[i+3]+code[i+4])!=2)
@@ -69,8 +67,6 @@ void posthet::check_code()
           }
     }
   else if(strlen(code_char)!=5)
-
-  // else if(code_char[5]!='\0'|| code_char[1]=='\0' || code_char[2]=='\0' ||code_char[3]=='\0' || code_char[4]=='\0' )
     {
       cout  <<"The code is invalid!\n";
       exit(1);

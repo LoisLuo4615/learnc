a59395ee4e0ce3f82f6d38538c61f43ba7a81d47
leaf <920733871@qq.com>
2018-01-16 19:58:52 +0800

    神经网络 基于 TensorFlow

--- python/neural_network.py
+++ python/neural_network.py
@@ -6,7 +6,7 @@
 import pandas as pd
 #使用0.19.2的pandas
 #参数初始化
-inputfile = 'E:/git/learn/python/data/sales_data.xls'
+inputfile = './data/sales_data.xls'
 data = pd.read_excel(inputfile, index_col = '序号') #导入数据
 
 #数据是类别标签，要将它转换为数据
@@ -28,13 +28,15 @@ model.add(Activation('relu')) #用relu函数作为激活函数，能够大幅提
 model.add(Dense(input_dim = 10, output_dim = 1))
 model.add(Activation('sigmoid')) #由于是0-1输出，用sigmoid函数作为激活函数
 
-model.compile(loss = 'binary_crossentropy', optimizer = 'adam', class_mode = 'binary')
+model.compile(loss = 'binary_crossentropy', optimizer = 'adam')#, class_mode = 'binary')
 #编译模型。由于我们做的是二元分类，所以我们指定损失函数为binary_crossentropy，以及模式为binary
 #另外常见的损失函数还有mean_squared_error、categorical_crossentropy等，请阅读帮助文件。
 #求解方法我们指定用adam，还有sgd、rmsprop等可选
 
 model.fit(x, y, nb_epoch = 1000, batch_size = 10) #训练模型，学习一千次
 yp = model.predict_classes(x).reshape(len(y)) #分类预测
+
+print(int(sum(yp==y))/len(y))##准确率0.76470
 
 from cm_plot import * #导入自行编写的混淆矩阵可视化函数
 cm_plot(y,yp).show() #显示混淆矩阵可视化结果
